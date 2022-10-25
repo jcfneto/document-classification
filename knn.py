554b080781663b732af6b6fab3f18ef875ee2fc5
jcfneto <eng.jcfneto@gmail.com>
2022-10-25 11:42:07 -0300

    knn results

--- knn.py
+++ knn.py
@@ -10,7 +10,7 @@ from utils import *
 # TODO: colocar em um arquivo de configuraćão depois.
 CV = 5
 KNN_PARAMS = {
-    'n_neighbors': range(3, 15),
+    'n_neighbors': range(3, 16),
     'weights': ('uniform', 'distance'),
     'metric': ('cosine', 'euclidean'),
 }
@@ -18,10 +18,13 @@ KNN_PARAMS = {
 KNN_PARAMS = list(itertools.product(*[KNN_PARAMS[k] for k in KNN_PARAMS]))
 
 
-if __name__ == '__main__':
+def main(df: pd.DataFrame, embedding_type: str) -> None:
+    """
 
-    train = pl.read_csv('output/tf_idf/train.csv').to_pandas()
-
+    Args:
+        df:
+        embedding_type:
+    """
     grid_search_results = {}
     kf = KFold(n_splits=CV, shuffle=True)
     for k, w, m in KNN_PARAMS:
@@ -39,8 +42,8 @@ if __name__ == '__main__':
         )
         grid_search_results[curr_search] = kfold(
             model,
-            train.iloc[:, 2:].values,
-            train.iloc[:, 0].values,
+            df.iloc[:, 2:].values,
+            df.iloc[:, 0].values,
             kf
         )
         exec_time = np.sum(grid_search_results[curr_search]['exec_time'])
@@ -51,6 +54,16 @@ if __name__ == '__main__':
         print(f'F1_micro: {np.round(micro, 3)}.')
         print(f'Execution time for {CV}-Fold: {np.round(exec_time, 3)}s.')
 
-    with open('output/results/knn/train_results.json', 'w') as of:
+    with open(f'output/results/knn/{embedding_type}_results.json', 'w') as of:
         json.dump(grid_search_results, of, cls=NumpyEncoder)
         print('Results are saved.')
+
+
+if __name__ == '__main__':
+
+    embeddings = ('tf_idf', 'bert')
+    for embedding in embeddings:
+        print(f'Starting grid search for "{embedding}" embedding.\n')
+        train = pl.read_csv(f'output/{embedding}/train.csv').to_pandas()
+        main(train, embedding)
+        print(f'Finish grid search for "{embeddings}".')
